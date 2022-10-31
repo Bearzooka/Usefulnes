e539b23122b258b230fa68fe65784b9a581b322a
Bernardo Prieto <bprieto@gmail.com>
2022-10-31 14:44:59 +0100

    Create OpenSSLVersions.sh

---
+++ OpenSSLVersions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+currentUser=$(ls -l /dev/console | awk {' print $3 '})
+
+createOpenSSLList (){
+    # Setting IFS Env to only use new lines as field seperator 
+    IFS=$'\n'
+    installs=$(su - "$currentUser" -c "type -a openssl")
+    for line in $installs; do
+        path=$(echo "$line" | awk -F"is " '{print $2}')
+        version=$($path version)
+        echo "$i;$path;$version"
+        ((i++))
+    done
+}
+result="$(createOpenSSLList)"
+
+echo "<result>$result</result>"
+
+exit 0
